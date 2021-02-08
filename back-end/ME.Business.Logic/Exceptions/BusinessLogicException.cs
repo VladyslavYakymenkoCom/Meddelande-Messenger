c824b945fbcf9115be9602565eba11d5b47182dd
YakymenkoVladyslavCom <vladyslav.yakymenko.ihorovych@gmail.com>
2021-02-09 00:53:43 +0200

    feat (back-end, global): business/forbidden exceptions add

---
+++ back-end/ME.Business.Logic/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ME.Business.Logic.Exceptions
+{
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(string message) : base(message)
+        {
+                
+        }
+    }
+}
---
+++ back-end/ME.Business.Logic/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace ME.Business.Logic.Exceptions
+{
+    public class ForbiddenException : BusinessLogicException
+    {
+        public ForbiddenException() : base("Action is forbidden.")
+        {
+
+        }
+    }
+}
