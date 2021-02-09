26643ca1d6ffae4c7d8c4ac361ed565323b7580c
YakymenkoVladyslavCom <vladyslav.yakymenko.ihorovych@gmail.com>
2021-02-10 00:05:55 +0200

    chore (back-end, global): api - clean-up

---
+++ back-end/ME.Api/Controllers/StateIndicatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ME.Api.Controllers
+{
+    [ApiController]
+    [Route("state-indicator")]
+    public class StateIndicatorController : ControllerBase
+    {
+        public StateIndicatorController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult State()
+        {
+            var response = "state: OK";
+
+            return new JsonResult(response);
+        }
+    }
+}
--- back-end/ME.Api/Startup.cs
+++ back-end/ME.Api/Startup.cs
@@ -1,19 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Mapster;
 using ME.Business.Logic.Scopes.Chats;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Meddelande
+namespace ME.Api
 {
     public class Startup
     {
